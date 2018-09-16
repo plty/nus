29c5ea6f40ee1757ffe8e17dde37da81175bb872
plty <platyunyu@gmail.com>
2018-09-17 03:27:43 +0800

    removed submodule

--- enrolled/parallel-CS3210/README.md
+++
@@ -1,1 +0,0 @@
-# nus-parallel-computing
--- enrolled/parallel-CS3210/labs/lab-2/runner-row.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..64}
-do
-    perf stat -o ex-4/stats-row --append -- ./mm-omp-row 2048 $i
-    perf stat -o ex-4/stats-row-cache --append -e cache-references -e cache-misses -e cycles -e instructions -- ./mm-omp-row 2048 $i
-done
