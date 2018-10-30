a53d1dfc1e68371c6c8a38429824868f7d970e2e
plty <platyunyu@gmail.com>
2018-10-30 14:23:11 +0800

    original modal of lab-4 and lab-5

---
+++ enrolled/parallel-CS3210/labs/lab-4/nblock_comm.c
@@ -0,0 +1,43 @@
+/**
+ * CS3210 - Non-blocking communication in MPI.
+ */
+
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int numtasks, rank, next, prev, tag1=1, tag2=2;
+	int buf[2];
+	
+	MPI_Request reqs[4];
+	MPI_Status stats[4];
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	prev = rank-1;
+	next = rank+1;
+	if (rank == 0)
+		prev = numtasks - 1;
+	
+	if (rank == (numtasks - 1))
+		next = 0;
+
+	MPI_Irecv(&buf[0], 1, MPI_INT, prev, tag1, MPI_COMM_WORLD, &reqs[0]);
+	MPI_Irecv(&buf[1], 1, MPI_INT, next, tag2, MPI_COMM_WORLD, &reqs[1]);
+
+	MPI_Isend(&rank, 1, MPI_INT, prev, tag2, MPI_COMM_WORLD, &reqs[2]);
+	MPI_Isend(&rank, 1, MPI_INT, next, tag1, MPI_COMM_WORLD, &reqs[3]);
+	
+	MPI_Waitall(4, reqs, stats);
+
+	printf("rank=%d received value=%d from rank=%d\n", rank, buf[0], prev);
+	printf("rank=%d received value=%d from rank=%d\n", rank, buf[1], next);
+
+	MPI_Finalize();
+	
+	return 0;
+}
+
