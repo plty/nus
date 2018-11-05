1c1a52d59234bcba316c72aeae69121608922e06
plty <platyunyu@gmail.com>
2018-11-06 00:41:16 +0800

    change to spaces

--- teaching/ds-algo-CS2040/kattis/shortestpath1/Dijkstra.java
+++ teaching/ds-algo-CS2040/kattis/shortestpath1/Dijkstra.java
@@ -1,61 +1,61 @@
 import java.util.*;
 
 public class Dijkstra {
-	private int size;
-	private ArrayList<ArrayList<Pair>> adl;
+    private int size;
+    private ArrayList<ArrayList<Pair>> adl;
 
-	public Dijkstra(int size) {
-		this.size = size;
-		adl = new ArrayList<>();
-		for (int i = 0; i < size; i++)
-			adl.add(new ArrayList<>());
-	}
+    public Dijkstra(int size) {
+        this.size = size;
+        adl = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+            adl.add(new ArrayList<>());
+    }
 
-	public void addEdge(int u, int v, int w) {
-		adl.get(u).add(new Pair(v, w));
-	}
+    public void addEdge(int u, int v, int w) {
+        adl.get(u).add(new Pair(v, w));
+    }
 
-	public ArrayList<Integer> shortestPath(int source) {
-		ArrayList<Integer> distance = new ArrayList<Integer>();
-		PriorityQueue<Pair> nearest = new PriorityQueue<>((a, b) -> a.w - b.w);
-		boolean[] visit = new boolean[this.size];
+    public ArrayList<Integer> shortestPath(int source) {
+        ArrayList<Integer> distance = new ArrayList<Integer>();
+        PriorityQueue<Pair> nearest = new PriorityQueue<>((a, b) -> a.w - b.w);
+        boolean[] visit = new boolean[this.size];
 
-		for (int i = 0; i < size; i++)
-			distance.add(-1);
+        for (int i = 0; i < size; i++)
+            distance.add(-1);
 
-		nearest.offer(new Pair(source, 0));
-		while (nearest.size() > 0) {
-			Pair cur = nearest.poll();
-			if (visit[cur.v])
-				continue;
+        nearest.offer(new Pair(source, 0));
+        while (nearest.size() > 0) {
+            Pair cur = nearest.poll();
+            if (visit[cur.v])
+                continue;
 
-			visit[cur.v] = true;
-			distance.set(cur.v, cur.w);
+            visit[cur.v] = true;
+            distance.set(cur.v, cur.w);
 
-			for (Pair ng: this.adl.get(cur.v))
-				nearest.offer(new Pair(ng.v, cur.w + ng.w));
-		}
-		return distance;
-	}
+            for (Pair ng: this.adl.get(cur.v))
+                nearest.offer(new Pair(ng.v, cur.w + ng.w));
+        }
+        return distance;
+    }
 
-	public static void main(String[] args) {
-	}
+    public static void main(String[] args) {
+    }
 }
 
 class Pair {
-	public int v;
-	public int w;
+    public int v;
+    public int w;
 
-	public Pair(int v, int w) {
-		this.v = v;
-		this.w = w;
-	}
+    public Pair(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
 
-	public Pair() {
-		this(0, 0);
-	}
+    public Pair() {
+        this(0, 0);
+    }
 
-	public String toString() {
-		return "(" + Integer.toString(v) + ", " + Integer.toString(w) + ")";
-	}
+    public String toString() {
+        return "(" + Integer.toString(v) + ", " + Integer.toString(w) + ")";
+    }
 }
--- teaching/ds-algo-CS2040/kattis/shortestpath1/Main.java
+++ teaching/ds-algo-CS2040/kattis/shortestpath1/Main.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n, m, q, s;
-		n = in.nextInt();
-		m = in.nextInt();
-		q = in.nextInt();
-		s = in.nextInt();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, m, q, s;
+        n = in.nextInt();
+        m = in.nextInt();
+        q = in.nextInt();
+        s = in.nextInt();
 
-		while (n + m + q + s > 0) {
-			Dijkstra dijkstra = new Dijkstra(n);
-			for (int i = 0; i < m; i++) {
-				int u = in.nextInt();
-				int v = in.nextInt();
-				int w = in.nextInt();
-				dijkstra.addEdge(u, v, w);
-			}
+        while (n + m + q + s > 0) {
+            Dijkstra dijkstra = new Dijkstra(n);
+            for (int i = 0; i < m; i++) {
+                int u = in.nextInt();
+                int v = in.nextInt();
+                int w = in.nextInt();
+                dijkstra.addEdge(u, v, w);
+            }
 
-			ArrayList<Integer> distance = dijkstra.shortestPath(s);
-			for (int i = 0; i < q; i++) {
-				int t = in.nextInt();
-				System.out.println(distance.get(t) == -1 ? "Impossible" : distance.get(t));
-			}
-			System.out.println();
+            ArrayList<Integer> distance = dijkstra.shortestPath(s);
+            for (int i = 0; i < q; i++) {
+                int t = in.nextInt();
+                System.out.println(distance.get(t) == -1 ? "Impossible" : distance.get(t));
+            }
+            System.out.println();
 
-			n = in.nextInt();
-			m = in.nextInt();
-			q = in.nextInt();
-			s = in.nextInt();
-		}
-	}
+            n = in.nextInt();
+            m = in.nextInt();
+            q = in.nextInt();
+            s = in.nextInt();
+        }
+    }
 }
